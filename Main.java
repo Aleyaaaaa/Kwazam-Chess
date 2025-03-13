53a87fe9054af83d978ac8981d807e77c3fef4db
Aleyaaaaa <aleyaanneesa@gmail.com>
2025-03-13 21:05:11 +0800

    delete all

--- Main.java
+++
@@ -1,9 +0,0 @@
-//Batrisya
-//starting point for the Kwazam chess game
-public class Main {
-    public static void main(String[] args) {
-        MenuView MenuView = new MenuView();
-        new MenuController(MenuView);
-    }
-}
-
